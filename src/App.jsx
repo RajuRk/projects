f84952de816facbcf347137488664a842f520ff7
Rajurk <rajurk044@gmail.com>
2024-01-05 17:57:40 +0530

    random color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Accordion from './components/accordion';
+import RandomColor from './components/random-color';
 
 function App() {
 
   return (
     <>
-      <Accordion/>
+      {/* <Accordion/> */}
+      <RandomColor/>
     </>
   )
 }
