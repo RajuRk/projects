017ebd84aeb4f01f28000b5dc69e0fecc3d19be2
Rajurk <rajurk044@gmail.com>
2024-01-10 14:04:30 +0530

    load more

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMore from './components/load-more-button';
 
 function App() {
 
@@ -11,11 +12,12 @@ function App() {
       {/* <Accordion/> */}
       {/* <RandomColor/> */}
       {/* <StarRating noOfStarts={10} /> */}
-      <ImageSlider 
+      {/* <ImageSlider 
         url={"https://picsum.photos/v2/list"} 
         page={"1"} 
         limit={"10"}
-      />
+      /> */}
+      <LoadMore/>
     </>
   )
 }
